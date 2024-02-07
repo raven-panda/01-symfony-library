49f0c621015e314ff29a8d6737f0de764fa6bace
Panda Rouge <ambroise.maire7@gmail.com>
2024-02-07 20:32:31 +0100

    add: created Author entity

---
+++ app-library/src/Entity/Author.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuthorRepository::class)]
+class Author
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 80)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $country = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $biography = null;
+
+    #[ORM\Column]
+    private ?bool $is_date_known = null;
+
+    #[ORM\Column]
+    private ?int $birthyear = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $birthdate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getBiography(): ?string
+    {
+        return $this->biography;
+    }
+
+    public function setBiography(string $biography): static
+    {
+        $this->biography = $biography;
+
+        return $this;
+    }
+
+    public function isIsDateKnown(): ?bool
+    {
+        return $this->is_date_known;
+    }
+
+    public function setIsDateKnown(bool $is_date_known): static
+    {
+        $this->is_date_known = $is_date_known;
+
+        return $this;
+    }
+
+    public function getBirthyear(): ?int
+    {
+        return $this->birthyear;
+    }
+
+    public function setBirthyear(int $birthyear): static
+    {
+        $this->birthyear = $birthyear;
+
+        return $this;
+    }
+
+    public function getBirthdate(): ?\DateTimeInterface
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate(\DateTimeInterface $birthdate): static
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+}
