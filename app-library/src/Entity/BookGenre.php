6e4b5cfa25fed86c64d8d09fdb018d2f90ef5c24
Panda Rouge <ambroise.maire7@gmail.com>
2024-02-07 20:33:25 +0100

    update: updated BookGenre entity for relationship with Book

--- app-library/src/Entity/BookGenre.php
+++ app-library/src/Entity/BookGenre.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\BookGenreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BookGenreRepository::class)]
@@ -18,6 +20,14 @@ class BookGenre
 
     #[ORM\Column(type: 'datetime_immutable', options: ['default' => 'CURRENT_TIMESTAMP'])]
     private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\ManyToMany(targetEntity: Book::class, mappedBy: 'genre')]
+    private Collection $books;
+
+    public function __construct()
+    {
+        $this->books = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +54,33 @@ class BookGenre
     public function setCreatedAt(\DateTimeImmutable $created_at): static
     {
         $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Book>
+     */
+    public function getBooks(): Collection
+    {
+        return $this->books;
+    }
+
+    public function addBook(Book $book): static
+    {
+        if (!$this->books->contains($book)) {
+            $this->books->add($book);
+            $book->addGenre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBook(Book $book): static
+    {
+        if ($this->books->removeElement($book)) {
+            $book->removeGenre($this);
+        }
 
         return $this;
     }
